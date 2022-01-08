8d2e9cf791d380176f528a5d9ebeb900dd53a0ce
mohammed <abdul.waheed9227@yahoo.com>
2022-01-08 11:17:32 -0800

    saving my first file

---
+++ mybash.sh
@@ -0,0 +1,6 @@
+PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:${PATH}"
+export PATH
+PATH=${PATH}:~/bin
+export PATH
+alias dc="pwd"
+alias nagios="ssh ubuntu@ec2-54-202-42-62.us-west-2.compute.amazonaws.com"
